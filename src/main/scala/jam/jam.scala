c22afbe7b397cf7329ff017a26253e52c7956f43
Bryan Shell <bryan.shell@orbitz.com>
2013-07-08 15:10:51 -0500

    Added Evan's changes

--- src/main/scala/jam/jam.scala
+++ src/main/scala/jam/jam.scala
@@ -1,5 +1,7 @@
 package jam
 
+import scala.util.matching.Regex
+
 object corrector {
 
   type Word = String
@@ -7,6 +9,21 @@ object corrector {
   type Correction = List[Word]
 
   type SplitWord = List[(Word, Word)]
+
+  val word = """[a-z]+""".r
+  val source = scala.io.Source.fromFile("/home/evan/lambda/spelling-jam/data/test1.txt")
+  val text = source.mkString
+  source.close()
+
+  def train(words:Iterable[String]) : Map[String,Int] = {
+    words.foldLeft(Map[String,Int] () withDefaultValue 0){
+      case (wordCount,inWord) =>
+        val lInWord = inWord.toLowerCase
+        wordCount + (lInWord -> (1 + wordCount(lInWord)))
+    }
+  }
+
+  val NWORDS = train(word.findAllIn(text).toIterable)
 
   val alphabet = "abcdefghijklmnopqrstuvwxyz"
 
@@ -51,4 +68,11 @@ object corrector {
       } yield  a ++ c ++ b
     }
   }
+
+  def known_edits2(word: String) : Set[String] = {
+    val edits = edits1(word)
+    val edits2 = edits.flatMap(edits1(_))
+      (edits ++ edits2).filter(NWORDS.contains(_))
+  }
+
 }
