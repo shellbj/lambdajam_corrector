45028fda441af76312554ca965ec8102b0a758bf
Bryan Shell <bryan.shell@orbitz.com>
2013-07-08 16:00:57 -0500

    Fix build issues

--- src/main/scala/jam/jam.scala
+++ src/main/scala/jam/jam.scala
@@ -20,17 +20,14 @@ object corrector {
 
   val alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-  def edits1(w: Word): Set[Word] = {
-    val s = toTuples(w)
-    Set(deletes(s) :: transposed(s) :: replaces(s) :: inserts(s))
-
-    def toTuples(w: String): List[(String, String)] =
+  def edits1(w: String): Set[String] = {
+    def toTuples(w: String): Seq[(String, String)] =
       for {
         i <- 0 to w.length
       } yield w.splitAt(i)
 
 
-    def deletes(s: List[(String, String)]): List[String] = {
+    def deletes(s: Seq[(String, String)]): Seq[String] = {
       for {
         (a, b) <- s
         if b != None
@@ -38,34 +35,34 @@ object corrector {
       } yield a ++ b.tail
     }
 
-    def transposed(s: List[(String, String)]): List[String] = {
-      for {
-        (a, b) <- s
-        if b.length > 1
-      } yield (((a ++ b.tail.head) ++ b.head) ++ b.drop(2))
+    def transposed(s: Seq[(String, String)]): Seq[String] = {
+      s.map({case (a,b) => a + b(1) + b(0) + b.drop(2)})
     }
 
-    def replaces(s: List[(String, String)]): List[String] = {
-      for {
-        (a, b) <- s
-        if b != None
-        if !b.isEmpty
-        c <- alphabet
-      } yield  a :: c :: b.tail
+    def replaces(s: Seq[(String, String)]): Seq[String] = {
+      alphabet.flatMap({case c => s.map({case (a,b) => a + c + b.tail})})
     }
 
-    def inserts(s: List[(String, String)]): List[String] = {
-      for {
-        (a, b) <- s
-        c <- alphabet
-      } yield  a ++ c ++ b
+    def inserts(s: Seq[(String, String)]): Seq[String] = {
+      alphabet.flatMap({case c => s.map({case (a,b) => a + c + b})})
     }
+
+    val s = toTuples(w)
+    Set() ++ deletes(s) ++ transposed(s) ++ replaces(s) ++ inserts(s)
   }
 
   def known_edits2(word: String) : Set[String] = {
-    val edits = edits1(word)
-    val edits2 = edits.flatMap(edits1(_))
-      (edits ++ edits2).filter(NWORDS.contains(_))
+    val edits2 = edits1(word).flatMap(edits1(_))
+    edits2.filter(NWORDS.contains(_))
   }
 
+  def known(words: Set[String]): Set[String] = {
+    words.filter(NWORDS.contains(_))
+  }
+
+  def correct(word: String): List[String] = {
+    val candidates = known(Set(word)) ++ known(edits1(word)) ++ known_edits2(word) ++ Set(word)
+    candidates.foldLeft(Map[String,Int] () withDefaultValue 0) {
+      case (wordCount,inWord) => wordCount + (inWord->NWORDS(inWord))}.toList.sortBy({case (k,v) => -v }).map({case (k,v) => k})
+  }
 }
